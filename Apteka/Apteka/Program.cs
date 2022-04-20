8bd67f18d74c55da36a0dcc667787d5d74b1af6c
KolyaBulanov <99391463+KolyaBulanov@users.noreply.github.com>
2022-04-20 15:39:49 +0300

    Add files via upload

---
+++ Apteka/Apteka/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+// Аптека
+public class Apteka
+{
+    // Название аптеки
+    public string Name { set; get; }
+
+    // Адрес
+    public string Location { set; get; }
+
+    // Препараты
+    public List<Preparat> med;
+}
+
+// Препараты
+public class Preparat
+{
+    // Название
+    public string PreparatName { set; get; }
+
+    // Цена
+    public float PreparatPrice { set; get; }
+   // public void Preparat(in int a)
+    
+}
+//Самое дорогое лекарство 
+public void MaxPrice()
+{
+    var res = float.MinValue;
+
+    foreach (var it in Preparat)
+    {
+        if (it.PreparatPrice < res) { res = it.PreparatPrice; }
+    }
+    return res;
+}
+
+// инициализация полей значениями по умолчанию - доступна с C#10
+
+//public string name = "Preparat";
+//public int age = 1;
